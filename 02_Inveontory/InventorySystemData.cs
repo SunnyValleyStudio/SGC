660758c78c7272dfcf0d765c70effbd6c5287ddf
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-08-25 20:12:35 +0200

    Inventory System Data - lecture 41

---
+++ 02_Inveontory/InventorySystemData.cs
@@ -0,0 +1,27 @@
+﻿using SVS.InventorySystem;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Inventory
+{
+    public class InventorySystemData
+    {
+        public Action updateHotbarCallback;
+        private Storage storagePlayer, storageHotbar;
+        List<int> inventoryUiElementId = new List<int>();
+        List<int> hotbarUiElementId = new List<int>();
+
+        public int selectedItemUIID = -1;
+
+        public InventorySystemData(int playerStroageSize, int hotbarStorageSize)
+        {
+            storagePlayer = new Storage(playerStroageSize);
+            storageHotbar = new Storage(hotbarStorageSize);
+        }
+
+
+    }
+}
+
