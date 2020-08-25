c94add0008bdc81dd14023ad57e6c4f85a9766a0
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-08-25 20:34:43 +0200

    Adding crafting ui p2 - lecture 107

---
+++ 05_CraftingSystem/IngredientItemElement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IngredientItemElement : ItemPanelHelper
+{
+    public Image panelImage;
+    public Text countNeedeTxt;
+
+    private void ModifyPanelAlpha(int value)
+    {
+        var panelColor = panelImage.color;
+        panelColor.a = Mathf.Clamp01(value);
+        panelImage.color = panelColor;
+    }
+
+    public void SetUnavailable()
+    {
+        ModifyPanelAlpha(1);
+    }
+
+    public void ResetAvailability()
+    {
+        ModifyPanelAlpha(0);
+    }
+
+    public override void SetItemUI(string name, Sprite image)
+    {
+        base.SetItemUI(name, image);
+        countNeedeTxt.text = "x 0";
+    }
+
+    public void SetItemUI(string name, Sprite image, int count, bool enoughItems)
+    {
+        base.SetItemUI(name, image);
+        countNeedeTxt.text = "x "+count;
+        if (enoughItems)
+        {
+            ResetAvailability();
+        }
+        else
+        {
+            SetUnavailable();
+        }
+    }
+}
--- 05_CraftingSystem/UiCrafting.cs
+++ 05_CraftingSystem/UiCrafting.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class UiCrafting : MonoBehaviour
 {
     public Action onCraftButtonClicked;
+    public Action<int> onRecipeClicked;
 
     private Dictionary<int, RecipeItemHelper> recipeUiElementDictionary = new Dictionary<int, RecipeItemHelper>();
 
@@ -67,5 +68,43 @@ public class UiCrafting : MonoBehaviour
     public void UnblockCraftButton()
     {
         craftBtn.interactable = true;
+    }
+
+    private void OnRecipeClicked(int id)
+    {
+        if (recipeUiElementDictionary.ContainsKey(id))
+        {
+            onRecipeClicked.Invoke(id);
+        }
+    }
+
+    public void ShowInventoryFull()
+    {
+        var element = Instantiate(inventoryFullText, Vector3.zero, Quaternion.identity, ingredientElementsPanel.transform);
+    }
+
+    public void AddIngredient(string ingredientName, Sprite ingredientSprite, int ingredientCOunt, bool enoughItems)
+    {
+        var element = Instantiate(ingredientPrefab, Vector3.zero, Quaternion.identity, ingredientElementsPanel.transform);
+        var ingredientHelper = element.GetComponent<IngredientItemElement>();
+        ingredientHelper.SetItemUI(ingredientName, ingredientSprite, ingredientCOunt, enoughItems);
+    }
+
+    public List<int> PrepareSRecipeItems(List<RecipeSO> listOfRecipes)
+    {
+        ClearUI();
+        recipeUiElementDictionary.Clear();
+        List<int> recipeUiIdList = new List<int>();
+        foreach (var item in listOfRecipes)
+        {
+            var element = Instantiate(recipePrefab, Vector3.zero, Quaternion.identity, recipesItemPanel.transform);
+            var recipeHelper = element.GetComponent<RecipeItemHelper>();
+            recipeHelper.OnClickEvent += OnRecipeClicked;
+            recipeUiElementDictionary.Add(element.GetInstanceID(), recipeHelper);
+            recipeHelper.SetItemUI(item.recipeName, item.GEtOutcomeSprite());
+
+            recipeUiIdList.Add(element.GetInstanceID());
+        }
+        return recipeUiIdList;
     }
 }
