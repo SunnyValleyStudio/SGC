d9f4cf9100cdc59f0b0a0bfbea6eba8891b61c9b
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-08-25 20:30:58 +0200

    Adding ambient sound - lecture 96

--- FootStepsSoundPlayer.cs
+++ FootStepsSoundPlayer.cs
@@ -5,25 +5,44 @@ using UnityEngine;
 public class FootStepsSoundPlayer : MonoBehaviour
 {
     public AudioSource footStepAudioSource;
-    public AudioClip footStepclip;
+    public AudioClip footStepclip, waterStepClip;
+    private AudioClip currentStepSound;
     float lastTime = 0;
     float duration;
+    public string waterTag;
 
     private void Start()
     {
         duration = footStepclip.length;
+        currentStepSound = footStepclip;
     }
 
     public void PlayFootstepSound()
     {
         if (lastTime == 0)
         {
-            footStepAudioSource.PlayOneShot(footStepclip);
+            footStepAudioSource.PlayOneShot(currentStepSound);
         }
         if(Time.time - lastTime >= duration)
         {
             lastTime = Time.time;
-            footStepAudioSource.PlayOneShot(footStepclip);
+            footStepAudioSource.PlayOneShot(currentStepSound);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "water")
+        {
+            currentStepSound = waterStepClip;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "water")
+        {
+            currentStepSound = footStepclip;
         }
     }
 }
