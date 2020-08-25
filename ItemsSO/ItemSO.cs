dc637abd083e326cb8eff8defff602639123afb0
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-08-25 20:11:44 +0200

    ItmSO  - lecture 38

---
+++ ItemsSO/ItemSO.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ItemSO : ScriptableObject
+{
+    public string ID;
+    public string itemName;
+    public Sprite imageSprite;
+    public GameObject model;
+    public bool isStackable;
+    [Range(1,100)]
+    public int stackLimit = 100;
+    public ItemType itemType;
+}
+
+public enum ItemType
+{
+    None,
+    Food,
+    Weapon
+}
