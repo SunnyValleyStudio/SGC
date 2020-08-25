c300d4d5c81535b0b94a989f65e3b21fb0adfd14
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-08-25 20:18:00 +0200

    Pickable item - lecture 53

---
+++ 03_PickingItems/IPickable.cs
@@ -0,0 +1,11 @@
+﻿using SVS.InventorySystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPickable
+{
+    IInventoryItem PickUp();
+
+    void SetCount(int value);
+}
---
+++ 03_PickingItems/PickableItem.cs
@@ -0,0 +1,42 @@
+﻿using SVS.InventorySystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableItem : MonoBehaviour, IPickable, IInventoryItem
+{
+    [SerializeField]
+    public ItemSO dataSource;
+    public int count;
+
+    public string ID => dataSource.ID;
+
+    public int Count { 
+        get 
+        {
+            if (dataSource.isStackable == false)
+            {
+                return 1;
+            }
+            return count;
+        } 
+    }
+
+    public bool IsStackable => dataSource.isStackable;
+
+    public int StackLimit => dataSource.stackLimit;
+
+    public IInventoryItem PickUp()
+    {
+        return this;
+    }
+
+    public void SetCount(int value)
+    {
+        count = value;
+        if (count == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
