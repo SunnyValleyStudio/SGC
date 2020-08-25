7a380405620547c97f6a9464ff282e5c80ce087c
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-08-25 20:11:59 +0200

    FoorItemSO - lecture 39

---
+++ ItemsSO/FoodItemSO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Generic Food Item", menuName ="InventoryData/FoodItemSO")]
+public class FoodItemSO : ItemSO
+{
+    public int staminaBonus = 0, hungerBonus = 0, energyBonus = 0;
+
+    public override bool IsUsable()
+    {
+        return true;
+    }
+
+}
--- ItemsSO/ItemSO.cs
+++ ItemsSO/ItemSO.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class ItemSO : ScriptableObject
+public abstract class ItemSO : ScriptableObject, ISerializationCallbackReceiver
 {
     public string ID;
     public string itemName;
@@ -12,6 +13,38 @@ public abstract class ItemSO : ScriptableObject
     [Range(1,100)]
     public int stackLimit = 100;
     public ItemType itemType;
+
+    public void OnAfterDeserialize()
+    {
+
+    }
+
+    public void OnBeforeSerialize()
+    {
+        if (string.IsNullOrEmpty(this.ID))
+        {
+            this.ID = Guid.NewGuid().ToString("N");
+        }
+        if (string.IsNullOrEmpty(itemName) && model != null)
+        {
+            itemName = model.name;
+        }
+    }
+
+    public Sprite GetImage()
+    {
+        return imageSprite;
+    }
+
+    public ItemType GetItemType()
+    {
+        return itemType;
+    }
+
+    public virtual bool IsUsable()
+    {
+        return false;
+    }
 }
 
 public enum ItemType
