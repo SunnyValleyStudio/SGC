45a61b365229485304cf41fe7a8fa7d9a79aa0a8
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-08-25 20:23:52 +0200

    Item spawner - lecture 74

--- 03_PickingItems/ItemSpawnManager.cs
+++ 03_PickingItems/ItemSpawnManager.cs
@@ -2,13 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ItemSpawnManager : MonoBehaviour
 {
-
     public static ItemSpawnManager instance;
     public Transform playerTransform;
     public string pickableLayerMask;
+
+    public Transform itemsSpawnersParent;
 
     private void Awake()
     {
@@ -20,6 +22,49 @@ public class ItemSpawnManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(SpawnAllItems());
+    }
+
+    private IEnumerator SpawnAllItems()
+    {
+        foreach (Transform itemSpawnerTransform in itemsSpawnersParent)
+        {
+            ItemSpawner spawner = itemSpawnerTransform.GetComponent<ItemSpawner>();
+            if (spawner != null)
+            {
+                Vector3 randomPosition = GenerateRandomPosition(spawner.radius);
+
+                if(spawner.singleObject && spawner.itemToSpawn.isStackable)
+                {
+                    CreateItemInPlace(itemSpawnerTransform.position + randomPosition, spawner.itemToSpawn, spawner.count);
+                }
+                else
+                {
+                    for (int i = 0; i < spawner.count; i++)
+                    {
+                        CreateItemInPlace(itemSpawnerTransform.position + randomPosition, spawner.itemToSpawn, spawner.count);
+                        randomPosition = GenerateRandomPosition(spawner.radius);
+                        
+                    }
+                }
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+
+    private void CreateItemInPlace(Vector3 randomPosition, ItemSO itemToSpawn, int count)
+    {
+        var itemGameObject = Instantiate(itemToSpawn.model, randomPosition, Quaternion.identity, itemsSpawnersParent);
+        PrepareItemGameObject(itemToSpawn.ID, count, itemGameObject);
+    }
+
+    private Vector3 GenerateRandomPosition(float radius)
+    {
+        return new Vector3(Random.Range(-radius, radius), Random.Range(0, radius), Random.Range(-radius, radius));
     }
 
     public void CreateItemAtPlayersFeet(string itemID, int currentItemCount)
---
+++ 03_PickingItems/ItemSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public ItemSO itemToSpawn;
+    [Range(1,100)]
+    public int count = 1;
+    public bool singleObject = false;
+
+    [Range(0.1f, 50f)]
+    public float radius = 1;
+    public bool showGizmo = true;
+    public Color gizmoColor = Color.green;
+
+    public void OnDrawGizmos()
+    {
+        if(showGizmo && radius > 0)
+        {
+            Gizmos.color = gizmoColor;
+            Gizmos.DrawWireSphere(transform.position, radius);
+        }
+    }
+}
