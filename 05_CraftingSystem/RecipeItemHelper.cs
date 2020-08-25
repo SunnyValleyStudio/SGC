3801f50c1d2ae5b1c9939d836663b259214ce818
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-08-25 20:32:59 +0200

    Recipe Item Panel Helper - lecture 103

---
+++ 05_CraftingSystem/RecipeItemHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RecipeItemHelper : ItemPanelHelper, IPointerClickHandler
+{
+    public Action<int> OnClickEvent;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        int id = gameObject.GetInstanceID();
+        OnClickEvent?.Invoke(id);
+    }
+}
