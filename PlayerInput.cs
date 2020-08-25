9f429b9e469ee569950fbfc357f8df33c20e547f
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-08-25 20:02:15 +0200

    lecture 8 - Getting player input

---
+++ PlayerInput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public Vector2 MovementInputVector { get; private set; }
+    public Vector3 MovementDirectionVector { get; private set; }
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        GetMovementInput();
+        GetMovementDirection();
+    }
+
+    private void GetMovementDirection()
+    {
+        MovementInputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Debug.Log(MovementInputVector);
+    }
+
+    private void GetMovementInput()
+    {
+        var cameraForewardDirection = mainCamera.transform.forward;
+        Debug.DrawRay(mainCamera.transform.position, cameraForewardDirection * 10, Color.red);
+        MovementDirectionVector = Vector3.Scale(cameraForewardDirection, (Vector3.right + Vector3.forward));
+        Debug.DrawRay(mainCamera.transform.position, MovementDirectionVector * 10, Color.green);
+    }
+}
