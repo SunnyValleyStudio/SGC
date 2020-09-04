933da6802b5467a20c17b9fc7a419377a0bc2a35
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-09-04 14:24:40 +0200

    Height difference check - Lecture 133

--- 06_PlacementSystem/PlacementHelper.cs
+++ 06_PlacementSystem/PlacementHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlacementHelper : MonoBehaviour
@@ -11,6 +12,14 @@ public class PlacementHelper : MonoBehaviour
     public List<Collider> collisions = new List<Collider>();
 
     private float raycastMaxDistance = 5f;
+    private float maxheightDifference = .3f;
+
+    LayerMask layerMask;
+
+    private void Start()
+    {
+        layerMask.value = 1 << LayerMask.NameToLayer("Ground");
+    }
 
     public void Initialize(Transform transform)
     {
@@ -46,11 +55,38 @@ public class PlacementHelper : MonoBehaviour
                 Debug.DrawRay(transform.TransformPoint(bottomLeftCorner) + Vector3.up, Vector3.down * raycastMaxDistance, Color.magenta);
                 Debug.DrawRay(transform.TransformPoint(bottomRightCorner) + Vector3.up, Vector3.down * raycastMaxDistance, Color.magenta);
 
+                RaycastHit hit1, hit2, hit3, hit4;
+                bool result1 = Physics.Raycast(transform.TransformPoint(topLeftCorner) + Vector3.up, Vector3.down, out hit1, raycastMaxDistance, layerMask);
+                bool result2 = Physics.Raycast(transform.TransformPoint(topRightCorner) + Vector3.up, Vector3.down, out hit2, raycastMaxDistance, layerMask);
+                bool result3 = Physics.Raycast(transform.TransformPoint(bottomLeftCorner) + Vector3.up, Vector3.down, out hit3, raycastMaxDistance, layerMask);
+                bool result4 = Physics.Raycast(transform.TransformPoint(bottomRightCorner) + Vector3.up, Vector3.down, out hit4, raycastMaxDistance, layerMask);
+
+                if(result1 && result2 && result3 && result4)
+                {
+                    float[] heightValuesList = { hit1.point.y, hit2.point.y , hit3.point.y , hit4.point.y };
+                    var min = heightValuesList.Min();
+                    var max = heightValuesList.Max();
+                    if(max-min > maxheightDifference)
+                    {
+                        Debug.Log("Too bigh height difference");
+                        ChangeMaterialColor(Color.red);
+                    }
+                    else
+                    {
+                        Debug.Log("Placement position correct");
+                        ChangeMaterialColor(Color.green);
+                    }
+                }
                 //check the height difference
 
                 //Change material color - user feedback
             }
         }
+    }
+
+    private void ChangeMaterialColor(Color color)
+    {
+
     }
 
     internal void DestroyStructure()
