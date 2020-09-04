1c8f797b15433c37d81ffa0bb817831c0b3445ed
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-09-04 14:24:14 +0200

    Logic behind terrain height detection - Lecture 132

--- 06_PlacementSystem/PlacementHelper.cs
+++ 06_PlacementSystem/PlacementHelper.cs
@@ -10,6 +10,8 @@ public class PlacementHelper : MonoBehaviour
     Rigidbody rb;
     public List<Collider> collisions = new List<Collider>();
 
+    private float raycastMaxDistance = 5f;
+
     public void Initialize(Transform transform)
     {
         playerTransform = transform;
@@ -27,6 +29,27 @@ public class PlacementHelper : MonoBehaviour
         {
             rb.position = playerTransform.position + playerTransform.forward;
             rb.MoveRotation(Quaternion.LookRotation(playerTransform.forward));
+            if(collisions.Count == 0)
+            {
+                //find corners of box collider
+                Vector3 bottomCenter = new Vector3(boxCollider.center.x, boxCollider.center.y - boxCollider.size.y / 2f,boxCollider.center.z);
+
+                Vector3 topLeftCorner = bottomCenter + new Vector3(-boxCollider.size.x / 2f, 0, boxCollider.size.z / 2f);
+                Vector3 topRightCorner = bottomCenter + new Vector3(+boxCollider.size.x / 2f, 0, boxCollider.size.z / 2f);
+                Vector3 bottomLeftCorner = bottomCenter + new Vector3(-boxCollider.size.x / 2f, 0, -boxCollider.size.z / 2f);
+                Vector3 bottomRightCorner = bottomCenter + new Vector3(boxCollider.size.x / 2f, 0, -boxCollider.size.z / 2f);
+
+                //shoot rays from thos points
+
+                Debug.DrawRay(transform.TransformPoint(topLeftCorner) + Vector3.up, Vector3.down * raycastMaxDistance, Color.magenta);
+                Debug.DrawRay(transform.TransformPoint(topRightCorner) + Vector3.up, Vector3.down * raycastMaxDistance, Color.magenta);
+                Debug.DrawRay(transform.TransformPoint(bottomLeftCorner) + Vector3.up, Vector3.down * raycastMaxDistance, Color.magenta);
+                Debug.DrawRay(transform.TransformPoint(bottomRightCorner) + Vector3.up, Vector3.down * raycastMaxDistance, Color.magenta);
+
+                //check the height difference
+
+                //Change material color - user feedback
+            }
         }
     }
 
