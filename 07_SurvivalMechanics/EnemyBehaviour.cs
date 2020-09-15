bf49e7db11a71d1abfa3fc107a37400df209a54d
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-09-15 09:34:39 +0200

    Enemy Animations - Lecture 151

---
+++ 07_SurvivalMechanics/EnemyBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public void Attack()
+    {
+
+    }
+
+    public void Die()
+    {
+
+    }
+}
