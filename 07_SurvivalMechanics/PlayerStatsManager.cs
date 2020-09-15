b123087efdc6cb3da8391ed9c114492dcd055590
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-09-15 09:33:02 +0200

    Player stats logic p1 - Lecture 148

---
+++ 07_SurvivalMechanics/PlayerStatsManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerStatsManager : MonoBehaviour
+{
+	[SerializeField]
+	public UnityEvent OnDeath;
+
+    [SerializeField]
+    private int healthInitialValue;
+    [SerializeField]
+    private int staminaInitialValue;
+
+	private float stamin;
+
+	public float Stamina
+	{
+		get { return stamin; }
+		set {
+			stamin = Mathf.Clamp(value, 0, staminaInitialValue);
+			uiPlayerStats.SetStamina(stamin / staminaInitialValue);
+		}
+	}
+
+	private float health;
+
+	public float Health
+	{
+		get { return health; }
+		set { 
+			health = Mathf.Clamp(value, 0, healthInitialValue);
+			uiPlayerStats.SetHealth(health / healthInitialValue);
+			if (health == 0)
+			{
+				Debug.Log("Dead");
+				OnDeath.Invoke();
+			}
+		}
+	}
+
+	public UiPlayerStats uiPlayerStats;
+
+	private void Awake()
+	{
+		Health = healthInitialValue;
+		Stamina = staminaInitialValue;
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			Health -= 40;
+			Stamina -= 10;
+		}
+	}
+
+}
---
+++ 07_SurvivalMechanics/UiPlayerStats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiPlayerStats : MonoBehaviour
+{
+    public Image healthBar, staminaBar;
+    
+    public void SetStamina(float value)
+    {
+        staminaBar.transform.localScale = new Vector3(Mathf.Clamp01(value), 1, 1);
+    }
+
+    public void SetHealth(float value)
+    {
+        healthBar.transform.localScale = new Vector3(Mathf.Clamp01(value), 1, 1);
+    }
+}
