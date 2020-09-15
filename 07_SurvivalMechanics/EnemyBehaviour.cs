8795cf8d8865bdb512681f6e71d2bccc104bcd60
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-09-15 09:36:15 +0200

    Enemy behaviour p2 - Lecture 153

--- 07_SurvivalMechanics/EnemyBehaviour.cs
+++ 07_SurvivalMechanics/EnemyBehaviour.cs
@@ -7,26 +7,40 @@ using UnityEngine.AI;
 public class EnemyBehaviour : MonoBehaviour
 {
     public EnemyPatrolArea enemyPatrolArea;
+    public NavMeshAgent navMehsAgent;
+    public GameObject target;
+    public float stoppinfgDistance = 2;
+    private bool playerInRange = false;
+
+    Animator animator;
+
     private void Start()
     {
         enemyPatrolArea.OnPlayerInRange += ChasePlayer;
         enemyPatrolArea.OnPlayerExit += StopChasing;
-
+        animator = GetComponent<Animator>();
+        navMehsAgent = GetComponent<NavMeshAgent>();
+        navMehsAgent.isStopped = true;
+        navMehsAgent.stoppingDistance = stoppinfgDistance;
     }
 
     private void StopChasing()
     {
-
+        animator.SetBool("Walk", false);
+        navMehsAgent.isStopped = true;
+        playerInRange = false;
     }
 
     private void ChasePlayer()
     {
-
+        animator.SetBool("Walk", true);
+        navMehsAgent.isStopped = false;
+        playerInRange = true;
     }
 
     public void Attack()
     {
-
+        target.GetComponent<AgentController>().playerStatsManager.Health -= 10;
     }
 
     public void Die()
@@ -34,4 +48,20 @@ public class EnemyBehaviour : MonoBehaviour
 
     }
 
+    private void Update()
+    {
+        if(navMehsAgent.isStopped == false)
+        {
+            navMehsAgent.SetDestination(target.transform.position);
+        }
+        if(playerInRange && navMehsAgent.velocity.sqrMagnitude == 0)
+        {
+            transform.LookAt(target.transform);
+            animator.SetBool("Attack", true);
+        }
+        else
+        {
+            animator.SetBool("Attack", false);
+        }
+    }
 }
